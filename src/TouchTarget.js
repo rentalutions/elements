b9966beb9b93a1054f9ac3fb903d3c5777979528
Patrick Krawczykowski <pkrawz@gmail.com>
2017-06-21 09:55:51 -0500

    added polished

--- src/TouchTarget.js
+++ src/TouchTarget.js
@@ -1,28 +1,45 @@
 import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { Link as RouterLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { darken } from 'polished'
 import { colors } from './variables'
 
-const Button = styled.button`
+const returnColor = (props) => (
+  props.primary ? colors.primary
+  : props.success ? colors.positive
+  : props.failure ? colors.negative
+  : props.warning ? colors.warning
+  : props.color ? props.color
+  : colors.feedback
+)
+
+const TouchTarget = ({
+  to,
+  className,
+  children,
+  ...rest
+}) => {
+  if (to) return <Link to={to} className={className} {...rest}>{children}</Link>
+  else return <button className={className} {...rest}>{children}</button>
+}
+
+const Button = styled(TouchTarget).attrs({
+  computedColor: returnColor(props)
+})`
   padding: 0.5em 1em;
-  background-color: ${({primary, success}) => primary ? colors.primary : 'transparent' };
-  border: 2px solid ${({color, primary, success}) => color ? color : colors.dark };
+  background-color: ${({computedColor, primary}) => primary ? computedColor : 'transparent'};
+  border: 2px solid ${({computedColor}) => computedColor};
   border-radius: 2px;
   cursor: pointer;
   font-size: 1rem;
+  text-transform: uppercase;
   &:hover {
-    background: ${colors.dark};
+    background: ${({computedColor, primary}) => primary ? darken(0.2, computedColor) : computedColor};
     color: ${colors.light};
   }
 `
+
 Button.displayName = 'Button'
 
-const Link = Button.withComponent(RouterLink)
-
-Link.displayName = 'Link'
-
-export default function TouchTarget (props) {
-  if (props.to) return Link
-  else return Button
-}
+export default Button
--- src/variables.js
+++ src/variables.js
@@ -7,11 +7,19 @@ export const sizes = {
 }
 
 export const colors = {
-  primary: 'rgba(101, 126, 212, 1)',
-  accent: 'rgba(255, 51, 31, 1)',
-  dark: 'rgba(13, 1, 6, 1)',
-  light: 'rgba(251, 251, 255, 1)',
-  darkSecondary: 'rgba(13, 1, 6, 0.6)',
-  lightSecondary: 'rgba(251, 251, 255, 0.8)',
-  disabled: 'rgba(13, 1, 6, 0.4)',
+  brand: {
+    primary: 'rgba(101, 126, 212, 1)',
+    accent: 'rgba(255, 51, 31, 1)',
+    dark: 'rgba(13, 1, 6, 1)',
+    light: 'rgba(251, 251, 255, 1)',
+    darkSecondary: 'rgba(13, 1, 6, 0.6)',
+    lightSecondary: 'rgba(251, 251, 255, 0.8)',
+    disabled: 'rgba(13, 1, 6, 0.4)',
+  },
+  ui: {
+    feedback: '#1FB6FF',
+    positive: '#1ABC9C',
+    negative: '#FF2D55',
+    warning: '#F5D76E',
+  }
 }
