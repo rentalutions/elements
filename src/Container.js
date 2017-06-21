95dee0853d29289ea26ae42b253c1827feecc39e
Patrick Krawczykowski <pkrawz@gmail.com>
2017-06-21 15:19:58 -0500

    build container, add padded prop

--- src/Container.js
+++ src/Container.js
@@ -2,8 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import { sizes } from './variables'
 
-const Container = styled.div`
+const Container = styled.div.attrs({
+  padding: props => props.padded ? '1em' : 'initial'
+})`
   max-width: sizes.xLarge;
+  padding: ${props => props.padding};
 `
 
 Container.displayName = 'Container'
