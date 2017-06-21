67a0f2d1ad5cfd0b40069d7a5fa4a475dd37c689
Patrick Krawczykowski <pkrawz@gmail.com>
2017-06-21 15:19:32 -0500

    clean up Card

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import { tint } from 'polished'
 import Text from './Text'
+import { colors } from './variables'
 
 const CardWrapper = styled.div`
   padding: 1em;
   background: ${
     ({bgImage, bgColor}) => (
-      bgImage ? `url(${bgImage})` : bgColor ? bgColor : '#F9F9F9'
+      bgImage ? `url(${bgImage})` : bgColor ? bgColor : colors.grey.light
     )
   };
   border-radius: 2px;
-  box-shadow: 0 1px 3px rgba(0,0,0,0.12);
+  box-shadow: 0 1px 3px ${tint(0.12, colors.grey.dark)};
+  > ${Text} {
+    margin-top: 0;
+    color: ${colors.grey.darkSecondary};
+  }
 `
 
 export default function Card ({
