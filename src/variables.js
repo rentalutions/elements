28feb6bb59555ac3343d6513a741aa8b5e031509
Patrick Krawczykowski <pkrawz@gmail.com>
2018-02-06 16:48:18 -0600

    convert variables to use context

--- src/variables.js
+++ src/variables.js
@@ -13,3 +13,58 @@ export const colors = {
   warning: "#F1B40F",
   external: "#7860C0"
 }
+
+const theme = {
+  colors: {
+    ui: {
+      dark: "#2F373D",
+      semiDark: "#6C757A",
+      main: "#ACB4B8",
+      semiLight: "#EBF1F5",
+      light: "#F9F8F8",
+    },
+    primary: {
+      dark: "#364046",
+      semiDark: "#7E8A91",
+      main: "#077DB8",
+      semiLight: "#DDE1E2",
+      light: "#D7EAF5",
+    },
+    external: {
+      dark: "#34113D",
+      semiDark: "#63057A",
+      main: "#9407B8",
+      semiLight: "#E9BAF5",
+      light: "#EFD7F5",
+    },
+    danger: {
+      dark: "#3D1311",
+      semiDark: "#990B06",
+      main: "#D6413C",
+      semiLight: "#F5BCBA",
+      light: "#F5D8D7",
+    },
+    warning: {
+      dark: "#3D3111",
+      semiDark: "#7A5C0A",
+      main: "#F5C60A",
+      semiLight: "#F5E5BA",
+      light: "#F2EAD5",
+    },
+    success: {
+      dark: "#023D34",
+      semiDark: "#057A69",
+      main: "#069985",
+      semiLight: "#A3D6CE",
+      light: "#D7F5F1",
+    }
+  },
+  sizing: 24,
+  fontSize: "16px",
+  fontFamily: "'Open Sans', Arial, sans-serif"
+}
+
+// color: ${({theme}) => theme.colors.ui.semiLight};
+// margin: ${({theme}) => theme.sizing * 3}px 0;
+
+export default theme
