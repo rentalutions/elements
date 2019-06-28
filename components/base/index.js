0895a95fc4b003b464cf71a16759180ae99e9584
Patrick Krawczykowski <patrick@dreadful.design>
2019-06-28 15:56:20 -0500

    add base styles

---
+++ components/base/index.js
@@ -0,0 +1,68 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+  :root {
+    font-size: 75%;
+  }
+
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: "Nunito", sans-serif;
+    font-size: 1.5rem;
+    line-height: 1.333;
+  }
+
+  html, body, div, span, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strong, sub, sup, var,
+  b, u, i, dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  h1,h2,h3,h4,h5 {
+    line-height: 1.5;
+  }
+
+  h1,
+  .hero {
+    font-size: 5rem;
+    font-weight: 200;
+  }
+  h2,
+  .headline {
+    font-size: 4rem;
+  }
+  h3,
+  .title {
+    font-size: 3rem;
+  }
+  h4,
+  .subtitle {
+    font-size: 2rem;
+  }
+  h5,
+  .emphasis {
+    font-size: 1.5rem;
+    font-weight: 700;
+  }
+`
