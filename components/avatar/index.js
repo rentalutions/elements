0eec2889f188ab9a8e188387c671bed485771dbe
Patrick Krawczykowski <patrick@dreadful.design>
2019-06-28 15:56:12 -0500

    add avatar

--- components/avatar/index.js
+++ components/avatar/index.js
@@ -1,40 +1,47 @@
 import React from "react"
-import styled from "styled-components"
+import styled, { css } from "styled-components"
+import { colors } from "../constants"
 
-const StyledAvatar = styled.div``
+const smallStyles = css`
+  width: 4rem;
+  > *:not(.initials) {
+    display: none;
+  }
+`
 
-export default function Avatar({
-  size = "md",
-  name = "",
-  photo_url = null,
-  initials = null,
-  email = null,
-  phone = null
-}) {
-  if (size === "sm")
+const StyledAvatar = styled.div`
+  position: relative;
+  padding: 1rem 2rem 1rem 1rem;
+  border-radius: 2rem;
+  background: ${colors.grey_300};
+  display: inline-flex;
+  align-items: center;
+  height: 4rem;
+  img {
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    object-fit: cover;
+    margin: 0;
+    padding: 0;
+  }
+  .name {
+    margin-left: 1rem;
+  }
+  ${({ small }) => small && smallStyles}
+`
+
+export default function Avatar({ small, name = "", photo_url = null, initials = "" }) {
+  if (small)
     return (
-      <StyledAvatar>
+      <StyledAvatar small>
         <p className="initials">{initials}</p>
-      </StyledAvatar>
-    )
-  if (size === "lg")
-    return (
-      <StyledAvatar>
-        <img src={photo_url} alt={`${name}'s profile photo`} className="profile-photo" />
-        <h2>{name}</h2>
-        <p>
-          {email} | {phone}
-        </p>
       </StyledAvatar>
     )
   return (
     <StyledAvatar>
-      <img
-        src={info.photo_url}
-        alt={`${name}'s profile photo`}
-        className="profile-photo"
-      />
-      <p>{name}</p>
+      <img src={photo_url} alt={`${name}'s profile photo`} className="profile-photo" />
+      <span className="name">{name}</span>
     </StyledAvatar>
   )
 }
