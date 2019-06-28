77cb5dd262ca196bcd3cba09c6b13d85d924f50b
Patrick Krawczykowski <patrick@dreadful.design>
2019-06-28 15:53:15 -0500

    remove storybook

--- stories/TextEditorStories.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-import TextEditor from "../src/TextEditor"
-
-export default storiesOf("TextEditor", module).add("Basic", () => (
-  <TextEditor />
-))
