528b3687ef63926bc6e746cb2961ddba96c035aa
Patrick Krawczykowski <patrick@dreadful.design>
2019-06-28 15:53:56 -0500

    add constants and update ignore

---
+++ components/constants.js
@@ -0,0 +1,27 @@
+export const colors = {
+  grey_100: "#ffffff",
+  grey_300: "#f3f3f3",
+  grey_500: "#c7c7c7",
+  grey_700: "#6f6f6f",
+  grey_900: "#2d2d2d",
+
+  blue_100: "#e9edf1",
+  blue_300: "#7c95b0",
+  blue_500: "#0f3e6f",
+  blue_700: "#0b2e51",
+  blue_900: "#0a2847",
+
+  gold_100: "#fdf9e9",
+  gold_300: "#fbeab3",
+  gold_500: "#f9de89",
+  gold_700: "#b6a264",
+  gold_900: "#443d26",
+
+  green_100: "#dff9f3",
+  green_300: "#9fefdd",
+  green_500: "#50e3c2",
+  green_700: "#3ba68e",
+  green_900: "#163e35",
+
+  red_500: "#ec4261"
+}
