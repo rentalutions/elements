164a6606938ba503490c6f5a11b9c6d41ff55199
Patrick Krawczykowski <patrick@dreadful.design>
2019-07-17 13:08:35 -0500

    add checkbox

--- components/checkbox/index.js
+++ components/checkbox/index.js
@@ -54,9 +54,9 @@ export default forwardRef(({ children, className, ...props }, ref) => (
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <polyline points="20 6 9 17 4 12" />
       </svg>
