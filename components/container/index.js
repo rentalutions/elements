2da49323e43f09d6bec6701e06b9fa8ecd96d6d0
Patrick Krawczykowski <patrick@dreadful.design>
2019-07-17 13:08:45 -0500

    add container

--- components/container/index.js
+++ components/container/index.js
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export default styled.section`
-  max-width: ${({ maxWidth = 86 }) => maxWidth}rem;
+  max-width: ${({ maxWidth = 84 }) => maxWidth}rem;
   margin-left: auto;
   margin-right: auto;
   padding-left: 2rem;
