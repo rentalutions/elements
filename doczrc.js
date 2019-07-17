e51c85059ee076911a23daf4cae5faaf02d2a05c
Patrick Krawczykowski <patrick@dreadful.design>
2019-07-17 10:36:55 -0500

    add documentation site and build step

---
+++ doczrc.js
@@ -0,0 +1,38 @@
+import { css } from "styled-components"
+
+export default {
+  title: "Avail Design Kit",
+  wrapper: "utils/wrapper.js",
+  menu: ["Getting Started", "Colors", { name: "Components", menu: ["Base"] }],
+  base: "/",
+  themeConfig: {
+    mode: "light",
+    showPlaygroundEditor: true,
+    fonts: {
+      display: "'Nunito', sans-serif"
+    },
+    colors: {
+      primary: "#0f3e6f",
+      grayUltraLight: "#f3f3f3",
+      grayExtraLight: "#c7c7c7",
+      grayLight: "#6f6f6f",
+      gray: "#2d2d2d"
+    },
+    codemirrorTheme: "material",
+    styles: {
+      playground: css`
+        padding: 4rem;
+      `
+    }
+  },
+  htmlContext: {
+    head: {
+      links: [
+        {
+          rel: "stylesheet",
+          href: "https://codemirror.net/theme/material.css"
+        }
+      ]
+    }
+  }
+}
