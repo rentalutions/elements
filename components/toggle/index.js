d4d6aed6d835a5c834341c40be92767eeb66a020
Patrick Krawczykowski <patrick@dreadful.design>
2019-07-17 13:09:08 -0500

    add toggle

---
+++ components/toggle/index.js
@@ -0,0 +1,65 @@
+import React, { forwardRef } from "react"
+import styled, { css } from "styled-components"
+import { colors } from "../constants"
+
+const StyledToggle = styled.label`
+  position: relative;
+  display: inline-flex;
+  cursor: pointer;
+  input {
+    display: none;
+    &:checked + .slider {
+      background-color: ${colors.blue_500};
+      &:before {
+        background-color: ${colors.grey_100};
+        border: 0.25rem solid ${colors.blue_500};
+        transform: translateX(2rem);
+      }
+    }
+  }
+  .slider {
+    position: relative;
+    height: 2rem;
+    width: 4rem;
+    background-color: ${colors.grey_500};
+    border-radius: 1rem;
+    transition: 200ms;
+    flex-shrink: 0;
+    &:before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      border-radius: 50%;
+      height: 2rem;
+      width: 2rem;
+      background-color: ${colors.grey_100};
+      border: 0.25rem solid ${colors.grey_500};
+      transition: 200ms;
+    }
+  }
+  .label {
+    margin-left: 1rem;
+  }
+  ${({ block }) => block && blockStyles}
+`
+
+const blockStyles = css`
+  display: flex;
+  flex-direction: row-reverse;
+  width: 100%;
+  .label {
+    margin-right: auto;
+    margin-left: 0;
+  }
+`
+
+export default forwardRef(({ className, children, block, ...props }, ref) => {
+  return (
+    <StyledToggle className={className} block={block}>
+      <input type="checkbox" {...props} ref={ref} />
+      <div className="slider" />
+      {children && <p className="label">{children}</p>}
+    </StyledToggle>
+  )
+})
