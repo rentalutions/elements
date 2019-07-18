97865177eb5cc4b3cbd54f0a607462f19ddb9619
Patrick Krawczykowski <patrick@dreadful.design>
2019-07-17 21:48:19 -0500

    update avatar

--- components/avatar/index.js
+++ components/avatar/index.js
@@ -1,6 +1,38 @@
 import React from "react"
 import styled, { css } from "styled-components"
 import { colors } from "../constants"
+import PropTypes from "prop-types"
+
+const StyledAvatar = styled.div`
+  position: relative;
+  border-radius: 2rem;
+  background: ${colors.ui_300};
+  display: inline-flex;
+  align-items: center;
+  height: 4rem;
+  img {
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+    object-fit: cover;
+    margin: 0;
+    padding: 0;
+    margin: 0.5rem;
+  }
+  .name {
+    margin-left: 1rem;
+    margin-right: 2rem;
+    overflow: hidden;
+    max-width: 20rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .initials {
+    text-align: center;
+    width: 100%;
+  }
+  ${({ small }) => small && smallStyles}
+`
 
 const smallStyles = css`
   width: 4rem;
@@ -9,41 +41,36 @@ const smallStyles = css`
   }
 `
 
-const StyledAvatar = styled.div`
-  position: relative;
-  padding: 1rem 2rem 1rem 1rem;
-  border-radius: 2rem;
-  background: ${colors.ui_300};
-  display: inline-flex;
-  align-items: center;
-  height: 4rem;
-  img {
-    width: 2rem;
-    height: 2rem;
-    border-radius: 50%;
-    object-fit: cover;
-    margin: 0;
-    padding: 0;
-  }
-  .name {
-    margin-left: 1rem;
-  }
-  ${({ small }) => small && smallStyles}
-`
-
-export default function Avatar({ small, name = "", photo_url = null, initials = "" }) {
-  if (small)
+export default function Avatar({
+  small = false,
+  name = "",
+  photoUrl = null,
+  initials = ""
+}) {
+  if (small) {
     return (
       <StyledAvatar small>
-        <p className="initials">{initials}</p>
+        <p className="initials">{initials.substring(0, 2)}</p>
       </StyledAvatar>
     )
+  }
   return (
     <StyledAvatar>
-      <img src={photo_url} alt={`${name}'s profile photo`} className="profile-photo" />
+      <img src={photoUrl} alt={`${name}'s profile photo`} className="profile-photo" />
       <span className="name">{name}</span>
     </StyledAvatar>
   )
+}
+
+Avatar.propTypes = {
+  /** Adjust the size of an avatar. Small removes the need for photos or names. */
+  small: PropTypes.bool,
+  /** User name, used in place of an email if present */
+  name: PropTypes.string,
+  /** User profile photo. Icon used by default */
+  photoUrl: PropTypes.string,
+  initials: PropTypes.string,
+  email: PropTypes.string
 }
 
 export const AvatarGroup = styled.div`
