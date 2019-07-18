cfe0519850e5815b76889654c04615238b3fce5c
Patrick Krawczykowski <patrick@dreadful.design>
2019-07-18 16:59:02 -0500

    update checkbox documentation

--- components/checkbox/index.js
+++ components/checkbox/index.js
@@ -43,24 +43,26 @@ const StyledCheckbox = styled.label`
   }
 `
 
-export default forwardRef(({ children, className, ...props }, ref) => (
-  <StyledCheckbox className={className}>
-    <input type="checkbox" ref={ref} {...props} />
-    <div className="input__target">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <polyline points="20 6 9 17 4 12" />
-      </svg>
-    </div>
-    {children && <span className="label">{children}</span>}
-  </StyledCheckbox>
-))
+export default forwardRef(({ children, className, ...props }, ref) => {
+  return (
+    <StyledCheckbox className={className}>
+      <input type="checkbox" ref={ref} {...props} />
+      <div className="input__target">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <polyline points="20 6 9 17 4 12" />
+        </svg>
+      </div>
+      {children && <span className="label">{children}</span>}
+    </StyledCheckbox>
+  )
+})
