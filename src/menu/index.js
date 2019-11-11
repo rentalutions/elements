5f59d7fbd22b411c4fde5b46c219ebc033f2430e
Patrick Krawczykowski <patrick@dreadful.design>
2019-11-11 15:41:12 -0600

    update menu structuree

---
+++ src/menu/index.js
@@ -0,0 +1,10 @@
+import React, {forwardRef} from "react"
+import styled from "styled-components"
+
+const StyledDropdown
+
+export default forwardRef((props, ref) => {
+  return <StyledDropdown {...props} ref={ref}>
+  {/* there she blows */}
+  </StyledDropdown>
+})
