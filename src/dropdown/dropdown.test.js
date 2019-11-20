87e9a869ef8191043187a9b0a466fb59696661a2
Patrick Krawczykowski <patrick@dreadful.design>
2019-11-20 10:40:42 -0600

    add test

---
+++ src/dropdown/dropdown.test.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import Dropdown from "src/dropdown"
+
+describe("<DropdownMenu />", () => {
+  it("Should not display the menu unless open.", () => {
+    expect(1).toEqual(1)
+  })
+})
