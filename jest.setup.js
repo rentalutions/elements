3e0ade5d9c88af45edd26427ee8c210d4d54f82b
Patrick Krawczykowski <patrick@dreadful.design>
2019-11-20 09:43:25 -0600

    quite unhelpful warning

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,17 @@
 import "@testing-library/jest-dom/extend-expect"
 import "@babel/polyfill"
+
+/**
+ * Suppress React 16.8 act() warnings globally.
+ * The react teams fix won't be out of alpha until 16.9.0.
+ */
+const consoleError = console.error
+beforeAll(() => {
+  jest.spyOn(console, "error").mockImplementation((...args) => {
+    if (
+      !args[0].includes("Warning: An update to %s inside a test was not wrapped in act")
+    ) {
+      consoleError(...args)
+    }
+  })
+})
