b522a34b2a0a423a769f69fdaabd1c5fb9254eaf
Patrick Krawczykowski <patrick@dreadful.design>
2019-11-20 09:24:39 -0600

    add tests for checkbox

---
+++ src/checkbox/checkbox.test.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { render, fireEvent } from "@testing-library/react"
+import Checkbox from "src/checkbox"
+
+describe("<Checkbox />", () => {
+  it("Should be checked on click.", () => {
+    const { getByTestId, container } = render(<Checkbox data-testid="checkbox" />)
+    const input = getByTestId("checkbox")
+    const element = container.firstChild
+    expect(input).not.toBeChecked()
+    fireEvent.click(element)
+    expect(input).toBeChecked()
+  })
+
+  it("Shouldn't be able to be checked when disabled.", () => {
+    const { getByTestId, container } = render(
+      <Checkbox data-testid="checkbox" disabled />
+    )
+    const input = getByTestId("checkbox")
+    const element = container.firstChild
+    fireEvent.click(element)
+    expect(input).not.toBeChecked()
+    expect(input).toBeDisabled()
+  })
+})
