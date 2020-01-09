858bea273f13f7859ff0fb8cd586464dc1bc6023
Patrick Krawczykowski <patrick@dreadful.design>
2020-01-09 16:17:04 -0600

    update test
    Update prop on tag test

--- src/tag/tag.test.js
+++ src/tag/tag.test.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, fireEvent } from "@testing-library/react"
+import { render, fireEvent, act } from "@testing-library/react"
 import Tag from "src/tag"
 
 describe("<Tag/>", () => {
@@ -7,12 +7,14 @@ describe("<Tag/>", () => {
     const { getByTestId } = render(<Tag data-testid="tag-test-id" />)
     expect(getByTestId("tag-test-id")).not.toBeNull()
   })
-  it("Should render a label if the filter prop is present.", () => {
-    const { getByTestId, container } = render(<Tag filter data-testid="filter-tag" />)
+  it("Should render a label if the filter prop is present.", async () => {
+    const { getByTestId, container } = render(<Tag select data-testid="filter-tag" />)
     const input = getByTestId("filter-tag")
     const element = container.firstChild
     expect(input).not.toBeChecked()
-    fireEvent.click(element)
+    await act(async () => {
+      fireEvent.click(element)
+    })
     expect(input).toBeChecked()
   })
 })
