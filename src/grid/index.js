153957d537cd0a7dae03f9b98c2dd6328201c816
Patrick Krawczykowski <patrick@dreadful.design>
2020-02-06 20:34:27 -0600

    update grid internals

--- src/grid/index.js
+++ src/grid/index.js
@@ -1,64 +1,6 @@
-import React, { forwardRef } from "react"
+import React, { forwardRef, Children, cloneElement } from "react"
 import styled from "styled-components"
-import PropTypes from "prop-types"
-
-const StyledGrid = styled.section`
-  display: grid;
-  grid-template-columns: repeat(${({ columns }) => columns}, 1fr);
-  grid-gap: ${({ gap }) => gap};
-`
-
-export const Grid = forwardRef(
-  (
-    {
-      children,
-      xsmBreak = "40rem",
-      smBreak = "60rem",
-      mdBreak = "80rem",
-      lgBreak = "100rem",
-      xlgBreak = "120rem",
-      ...props
-    },
-    ref
-  ) => {
-    const renderChildren = () =>
-      React.Children.map(children, child =>
-        React.cloneElement(child, {
-          xsmBreak,
-          smBreak,
-          mdBreak,
-          lgBreak,
-          xlgBreak
-        })
-      )
-    return (
-      <StyledGrid ref={ref} {...props}>
-        {renderChildren()}
-      </StyledGrid>
-    )
-  }
-)
-
-Grid.propTypes = {
-  columns: PropTypes.number,
-  gap: PropTypes.string,
-  xsmBreak: PropTypes.string,
-  smBreak: PropTypes.string,
-  mdBreak: PropTypes.string,
-  lgBreak: PropTypes.string,
-  xlgBreak: PropTypes.string,
-  children: PropTypes.node.isRequired
-}
-
-Grid.defaultProps = {
-  xsmBreak: "40rem",
-  smBreak: "60rem",
-  mdBreak: "80rem",
-  lgBreak: "100rem",
-  xlgBreak: "120rem",
-  gap: "2rem",
-  columns: 12
-}
+import p from "prop-types"
 
 function getIndex(arr, index) {
   return arr[index] ? arr[index] : arr[arr.length - 1]
@@ -69,40 +11,72 @@ function getOffset(offset, index) {
   return start === "auto" ? "auto" : start + 1
 }
 
-export const Col = styled.div`
-  grid-column: ${({ offset }) => `${getOffset(offset, 0)} /`} span
-    ${({ span }) => getIndex(span, 0)};
-  grid-row: ${({ offsetRow }) => `${getOffset(offsetRow, 0)} /`} span
-    ${({ spanRow }) => getIndex(spanRow, 0)};
-  @media (min-width: ${({ xsmBreak }) => xsmBreak}) {
-    grid-column: ${({ offset }) => `${getOffset(offset, 1)} /`} span
-      ${({ span }) => getIndex(span, 1)};
-    grid-row: ${({ offsetRow }) => `${getOffset(offsetRow, 1)} /`} span
-      ${({ spanRow }) => getIndex(spanRow, 1)};
+const StyledGrid = styled.section`
+  display: grid;
+  grid-template-columns: repeat(${({ theme: { columns } }) => columns}, 1fr);
+  grid-gap: ${({ theme: { gap } }) => gap}rem;
+`
+
+function GridContainer(
+  {
+    children,
+    theme = {
+      columns: 12,
+      gap: 2,
+      breakPoints: {
+        xsm: 40,
+        sm: 60,
+        md: 80,
+        lg: 100,
+        xlg: 120
+      }
+    },
+    ...props
+  },
+  ref
+) {
+  const childProps = { ...theme.breakPoints }
+  return (
+    <StyledGrid ref={ref} theme={theme} {...props}>
+      {Children.map(children, child => cloneElement(child, childProps))}
+    </StyledGrid>
+  )
+}
+
+const Col = styled.div`
+  ${({ offset, span, offsetRow, spanRow }) => `
+    grid-column: ${getOffset(offset, 0)} / span ${getIndex(span, 0)};
+    grid-row: ${getOffset(offsetRow, 0)} / span ${getIndex(spanRow, 0)};
+  `}
+  @media (min-width: ${({ xsm }) => xsm}rem) {
+    ${({ offset, span, offsetRow, spanRow }) => `
+      grid-column: ${getOffset(offset, 1)} / span ${getIndex(span, 1)};
+      grid-row: ${getOffset(offsetRow, 1)} / span ${getIndex(spanRow, 1)};
+    `}
   }
-  @media (min-width: ${({ smBreak }) => smBreak}) {
-    grid-column: ${({ offset }) => `${getOffset(offset, 2)} /`} span
-      ${({ span }) => getIndex(span, 2)};
-    grid-row: ${({ offsetRow }) => `${getOffset(offsetRow, 2)} /`} span
-      ${({ spanRow }) => getIndex(spanRow, 2)};
+  @media (min-width: ${({ sm }) => sm}rem) {
+    ${({ offset, span, offsetRow, spanRow }) => `
+      grid-column: ${getOffset(offset, 2)} / span ${getIndex(span, 2)};
+      grid-row: ${getOffset(offsetRow, 2)} / span ${getIndex(spanRow, 2)};
+    `}
   }
-  @media (min-width: ${({ mdBreak }) => mdBreak}) {
-    grid-column: ${({ offset }) => `${getOffset(offset, 3)} /`} span
-      ${({ span }) => getIndex(span, 3)};
-    grid-row: ${({ offsetRow }) => `${getOffset(offsetRow, 3)} /`} span
-      ${({ spanRow }) => getIndex(spanRow, 3)};
+  @media (min-width: ${({ md }) => md}rem) {
+    ${({ offset, span, offsetRow, spanRow }) => `
+      grid-column: ${getOffset(offset, 3)} / span ${getIndex(span, 3)};
+      grid-row: ${getOffset(offsetRow, 3)} / span ${getIndex(spanRow, 3)};
+    `}
   }
-  @media (min-width: ${({ lgBreak }) => lgBreak}) {
-    grid-column: ${({ offset }) => `${getOffset(offset, 4)} /`} span
-      ${({ span }) => getIndex(span, 4)};
-    grid-row: ${({ offsetRow }) => `${getOffset(offsetRow, 4)} /`} span
-      ${({ spanRow }) => getIndex(spanRow, 4)};
+  @media (min-width: ${({ lg }) => lg}rem) {
+    ${({ offset, span, offsetRow, spanRow }) => `
+      grid-column: ${getOffset(offset, 4)} / span ${getIndex(span, 4)};
+      grid-row: ${getOffset(offsetRow, 4)} / span ${getIndex(spanRow, 4)};
+    `}
   }
-  @media (min-width: ${({ xlgBreak }) => xlgBreak}) {
-    grid-column: ${({ offset }) => `${getOffset(offset, 5)} /`} span
-      ${({ span }) => getIndex(span, 5)};
-    grid-row: ${({ offsetRow }) => `${getOffset(offsetRow, 5)} /`} span
-      ${({ spanRow }) => getIndex(spanRow, 5)};
+  @media (min-width: ${({ xlg }) => xlg}rem) {
+    ${({ offset, span, offsetRow, spanRow }) => `
+      grid-column: ${getOffset(offset, 5)} / span ${getIndex(span, 5)};
+      grid-row: ${getOffset(offsetRow, 5)} / span ${getIndex(spanRow, 5)};
+    `}
   }
 `
 
@@ -114,8 +88,12 @@ Col.defaultProps = {
 }
 
 Col.propTypes = {
-  span: PropTypes.arrayOf(PropTypes.number),
-  spanRow: PropTypes.arrayOf(PropTypes.number),
-  offset: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number])),
-  offsetRow: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number]))
+  span: p.arrayOf(p.number),
+  spanRow: p.arrayOf(p.number),
+  offset: p.arrayOf(p.oneOfType([p.string, p.number])),
+  offsetRow: p.arrayOf(p.oneOfType([p.string, p.number]))
 }
+
+const Grid = forwardRef(GridContainer)
+
+export { Grid, Col }
