f25fe16ad1a1e65d22bf3e304a23c5dc08fc9a32
Patrick Krawczykowski <patrick@dreadful.design>
2020-04-06 09:51:38 -0500

    add storybook

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,4 @@
 module.exports = {
   presets: ["@babel/preset-env", "@babel/preset-react"],
-  plugins: [
-    "styled-components",
-    [
-      "module-resolver",
-      {
-        alias: {
-          src: "./src",
-          utils: "./utils",
-        },
-      },
-    ],
-  ],
+  plugins: ["styled-components"],
 }
---
+++ packages/elements/src/avatar/avatar.stories.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Avatar from "./index"
+
+export default { title: "Avatar" }
+
+export function Basic() {
+  return (
+    <Avatar
+      name="Janet Wood"
+      photoUrl="https://source.unsplash.com/collection/802865/48x48"
+    />
+  )
+}
+
+export function Small() {
+  return <Avatar small initials="JW" />
+}
+
+export function WithIcon() {
+  return (
+    <Avatar
+      name="Janet Wood"
+      photoUrl="https://source.unsplash.com/collection/802865/48x48"
+      icon={ExternalLink}
+    />
+  )
+}
