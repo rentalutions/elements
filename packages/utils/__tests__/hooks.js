6836415d83281c91c639723c76599e0fdf8fe3af
Patrick Krawczykowski <patrick@dreadful.design>
2020-04-17 13:50:10 -0500

    update hooks test

--- packages/utils/__tests__/hooks.js
+++ packages/utils/__tests__/hooks.js
@@ -1,5 +1,5 @@
 import { renderHook } from "@testing-library/react-hooks"
-import { useIntersection, useResize, usePortal } from "./src"
+import { useIntersection, useResize, usePortal } from "../src"
 
 describe("useIntersection", () => {
   const {
