32a964f244f6c1d9f838fc45193eb9480482d69f
pkrawc <patrick@dreadful.design>
2020-05-16 15:45:23 -0500

    update stack to use flex instead of grid

--- packages/layout/src/stack.js
+++ packages/layout/src/stack.js
@@ -4,24 +4,17 @@ import React, {
   isValidElement,
   cloneElement,
 } from "react"
-import Box from "./box"
+import Flex from "./flex"
 
 function Stack({ children, row = false, spacing = "2rem", ...props }, ref) {
   const validChildren = Children.toArray(children).filter(isValidElement)
+  const spaceProps = row ? { mr: spacing } : { mb: spacing }
   return (
-    <Box
-      {...props}
-      display="flex"
-      flexDirection={row ? "row" : "column"}
-      ref={ref}
-    >
+    <Flex {...props} flexDirection={row ? "row" : "column"} ref={ref}>
       {validChildren.map((child, idx) =>
-        cloneElement(
-          child,
-          row ? { mr: spacing, key: idx } : { mb: spacing, key: idx }
-        )
+        cloneElement(child, { key: idx, ...spaceProps })
       )}
-    </Box>
+    </Flex>
   )
 }
 
