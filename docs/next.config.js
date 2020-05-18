3877aaeb45e320aed0c112d674d41a63a5fa6024
pkrawc <patrick@dreadful.design>
2020-05-18 15:18:49 -0500

    add feedback package

--- docs/next.config.js
+++ docs/next.config.js
@@ -4,7 +4,7 @@ const compose = require("next-compose-plugins")
 
 const mdx = withMdxEnhanced({
   defaultLayout: true,
-  fileExtensions: ["mdx"]
+  fileExtensions: ["mdx"],
 })
 
 const transpile = withTranspile([
@@ -12,6 +12,7 @@ const transpile = withTranspile([
   "@rent_avail/base",
   "@rent_avail/controls",
   "@rent_avail/dialog",
+  "@rent_avail/feedback",
   "@rent_avail/input",
   "@rent_avail/layout",
   "@rent_avail/menu",
@@ -21,9 +22,9 @@ const transpile = withTranspile([
   "@rent_avail/tag",
   "@rent_avail/tooltip",
   "@rent_avail/typography",
-  "@rent_avail/utils"
+  "@rent_avail/utils",
 ])
 
 module.exports = compose([mdx, transpile], {
-  pageExtensions: ["mdx", "js"]
+  pageExtensions: ["mdx", "js"],
 })
