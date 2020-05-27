35a3b5dfe5696a0820430c297c4e4910e032ac3c
pkrawc <patrick@dreadful.design>
2020-05-27 13:42:33 -0500

    remove unneeded babel plugin

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   presets: [
     ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-react",
+    "@babel/preset-react"
   ],
   plugins: [
-    "@babel/plugin-proposal-optional-chaining",
     ["styled-components", { ssr: true }],
-    ["module-resolver", { alias: { "testing-utils": "./testing-utils.js" } }],
-  ],
+    ["module-resolver", { alias: { "testing-utils": "./testing-utils.js" } }]
+  ]
 }
