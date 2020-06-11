13aed45839c7f2a73c71f76566a305417c8b3d7d
pkrawc <patrick@dreadful.design>
2020-06-11 14:38:19 -0500

    remove transpile on node_module

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,5 +1,5 @@
 const withMdxEnhanced = require("next-mdx-enhanced")
-const withTranspile = require("next-transpile-modules")
+// const withTranspile = require("next-transpile-modules")
 const compose = require("next-compose-plugins")
 
 const mdx = withMdxEnhanced({
@@ -7,23 +7,23 @@ const mdx = withMdxEnhanced({
   fileExtensions: ["mdx"],
 })
 
-const transpile = withTranspile([
-  "@rent_avail/avatar",
-  "@rent_avail/base",
-  "@rent_avail/controls",
-  "@rent_avail/dialog",
-  "@rent_avail/feedback",
-  "@rent_avail/input",
-  "@rent_avail/layout",
-  "@rent_avail/menu",
-  "@rent_avail/popover",
-  "@rent_avail/progress",
-  "@rent_avail/select",
-  "@rent_avail/tag",
-  "@rent_avail/tooltip",
-  "@rent_avail/typography",
-  "@rent_avail/utils",
-])
+// const transpile = withTranspile([
+//   "@rent_avail/avatar",
+//   "@rent_avail/base",
+//   "@rent_avail/controls",
+//   "@rent_avail/dialog",
+//   "@rent_avail/feedback",
+//   "@rent_avail/input",
+//   "@rent_avail/layout",
+//   "@rent_avail/menu",
+//   "@rent_avail/popover",
+//   "@rent_avail/progress",
+//   "@rent_avail/select",
+//   "@rent_avail/tag",
+//   "@rent_avail/tooltip",
+//   "@rent_avail/typography",
+//   "@rent_avail/utils",
+// ])
 
 module.exports = compose([mdx], {
   pageExtensions: ["mdx", "js"],
