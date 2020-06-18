f4146f982841a2b47d4ba6fb804217e58aea5103
pkrawc <patrick@dreadful.design>
2020-06-18 08:59:15 -0500

    update docs build

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,5 +1,4 @@
 const withMdxEnhanced = require("next-mdx-enhanced")
-const withTranspile = require("next-transpile-modules")
 const compose = require("next-compose-plugins")
 
 const mdx = withMdxEnhanced({
@@ -7,24 +6,6 @@ const mdx = withMdxEnhanced({
   fileExtensions: ["mdx"]
 })
 
-const transpile = withTranspile([
-  "@rent_avail/avatar",
-  "@rent_avail/base",
-  "@rent_avail/controls",
-  "@rent_avail/dialog",
-  "@rent_avail/feedback",
-  "@rent_avail/input",
-  "@rent_avail/layout",
-  "@rent_avail/menu",
-  "@rent_avail/popover",
-  "@rent_avail/progress",
-  "@rent_avail/select",
-  "@rent_avail/tag",
-  "@rent_avail/tooltip",
-  "@rent_avail/typography",
-  "@rent_avail/utils"
-])
-
-module.exports = compose([transpile, mdx], {
+module.exports = compose([mdx], {
   pageExtensions: ["mdx", "js"]
 })
