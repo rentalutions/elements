b70b21caddbd2f2c0b4ed62166f348bd130784b5
Patrick Krawczykowski <patrick@dreadful.design>
2020-07-06 08:39:53 -0500

    update jest to use packages feature

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  plugins: [
-    ["styled-components", { ssr: true }],
-    ["module-resolver", { alias: { "testing-utils": "./testing-utils.js" } }],
-  ],
+  plugins: [["styled-components", { ssr: true }]],
   env: {
     test: {
-      plugins: ["@babel/plugin-transform-runtime"],
+      plugins: [
+        [
+          "module-resolver",
+          { alias: { "testing-utils": "./testing-utils.js" } },
+        ],
+      ],
     },
   },
 }
