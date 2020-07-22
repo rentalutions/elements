b55b3725a7d0a8f97c1639d20496e460300f71ed
Patrick Krawczykowski <patrick@dreadful.design>
2020-07-22 15:59:34 -0500

    build(dialog): trying to fix a weird build failure

--- packages/dialog/src/index.js
+++ packages/dialog/src/index.js
@@ -1,4 +1,4 @@
 export { Dialog, DialogTarget } from "./dialogContext"
 export { FullscreenDialog } from "./Fullscreen"
-export { ConfirmationDialog } from "./Confirmation"
+export { ConfirmationDialog } from "./Confirm"
 export { DialogHeader } from "./Header"
