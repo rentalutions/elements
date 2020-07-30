87a05fc95d34c3e8a939dec50a5df07851e2807f
Patrick Krawczykowski <patrick@dreadful.design>
2020-07-30 18:48:18 -0500

    fix: correct the regular expression

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   testPathIgnorePatterns: ["/node_modules/"],
-  transformIgnorePatterns: ["/node_modules/", "<rootDir>/packages/**/dist/"],
+  transformIgnorePatterns: ["/node_modules/", "<rootDir>/packages/*./dist/"],
 }
