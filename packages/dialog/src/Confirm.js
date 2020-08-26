0a7bd2c3c8491a94db1de1b5e27da1e80da9d82f
Dmytro Bubon <dmitry.bubon@gmail.com>
2020-08-26 16:09:10 +0300

    Remove onAnimationComplete from confirmation Dialog

--- packages/dialog/src/Confirm.js
+++ packages/dialog/src/Confirm.js
@@ -63,7 +63,6 @@ function Confirmation({ children, title = null, ...props }, ref) {
               initial={{ opacity: 0, scale: 1.05, x: "-50%" }}
               animate={{ opacity: 1, scale: 1, transition: { delay: 0.3 } }}
               exit={{ opacity: 0, scale: 1.05 }}
-              onAnimationComplete={(e) => console.log("complete")}
             >
               {children}
             </ConfirmationWrapper>
