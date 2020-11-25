a601da70367f1a841cdc858c732183efbdb16721
Patrick Krawczykowski <patrick@dreadful.design>
2020-11-25 10:58:05 -0600

    fix: refactor input component with wrapper
    Connor was right initially, we'll need a wrapper to implement the clip-path styling for the textarea variant.
    
    BREAKING CHANGE: ID was being swallowed by the component

--- packages/input/src/index.js
+++ packages/input/src/index.js
@@ -11,7 +11,6 @@ function Input(
     defaultValue,
     disabled,
     error,
-    id,
     icon,
     label,
     labelId,
@@ -37,134 +36,139 @@ function Input(
     setFilled(value?.length || isDate)
   }, [value])
   return (
-    <Box
-      as="label"
-      className={clsx(className, { filled, error })}
-      sx={{
-        position: "relative",
-        display: "block",
-        minHeight: "9rem",
-        color: disabled ? "ui_700" : "ui_900",
-        cursor: disabled ? "not-allowed" : "text",
-        lineHeight: "small",
-        width: "100%",
-        "&:focus-within .input__label-row, &.filled .input__label-row": {
-          transform: "translateY(-1rem) scale(0.889)",
-        },
-        "&.filled:not(:focus-within):not(.error) .input__label-row": {
-          color: disabled ? "ui_500" : "ui_700",
-        },
-        "&.error .input__label-row": {
-          color: "red_500",
-        },
-        "& > *": {
-          transition: "120ms",
-        },
-        ...sx,
-      }}
-    >
+    <Box sx={{ minHeight: "9rem" }} className={className}>
       <Box
-        {...props}
-        ref={ref}
-        as={as}
-        type={type}
-        aria-labelledby={ariaId}
-        value={value}
-        defaultValue={defaultValue}
-        disabled={disabled}
-        onChange={wrapEvent(onChange, handleChange)}
-        placeholder={isDate ? "mm/dd/yyyy" : undefined}
-        className={clsx({ error })}
+        as="label"
+        className={clsx({ filled, error })}
         sx={{
-          all: "unset",
+          position: "relative",
+          display: "block",
+          color: disabled ? "ui_700" : "ui_900",
+          cursor: disabled ? "not-allowed" : "text",
+          lineHeight: "small",
+          width: "100%",
           borderWidth: 2,
           borderStyle: "solid",
           borderColor: "ui_500",
           borderRadius: 4,
-          appearance: "none",
-          boxSizing: "border-box",
-          position: "relative",
-          p: icon ? "3rem 2rem 1rem 5rem" : "3rem 2rem 1rem",
-          fontFamily: "body",
-          height: isTextarea ? "auto" : "6.5rem",
-          width: "100%",
-          "&.error": {
-            borderColor: "red_500",
-          },
           "&:focus-within": {
             borderColor: "blue_500",
             color: "blue_500",
           },
-          "&::-webkit-calendar-picker-indicator": {
-            background: "transparent",
-            opacity: 1,
-            cursor: "pointer",
+          "&:focus-within .input__label-row, &.filled .input__label-row": {
+            transform: "translateY(-1rem) scale(0.889)",
           },
-          "&::-webkit-calendar-picker-indicator:hover + svg": {
-            color: "ui_300",
+          "&.filled:not(:focus-within):not(.error) .input__label-row": {
+            color: disabled ? "ui_500" : "ui_700",
           },
-        }}
-      />
-      {isDate && (
-        <Box
-          as={Calendar}
-          className="calendar-icon"
-          sx={{
-            pointerEvents: "none",
-            position: "absolute",
-            right: "2rem",
-            top: "3rem",
-          }}
-        />
-      )}
-      <Box
-        className="input__label-row"
-        sx={{
-          position: "absolute",
-          display: "flex",
-          alignItems: "center",
-          top: "2.25rem",
-          left: icon ? "5rem" : "2rem",
-          transformOrigin: "top left",
-          pointerEvents: "none",
-          color: disabled ? "ui_500" : "inherit",
+          "&.error": {
+            borderColor: "red_500",
+            color: "ui_900",
+          },
+          "&.error .input__label-row": {
+            color: "red_500",
+          },
+          "& > *": {
+            transition: "120ms",
+          },
+          ...sx,
         }}
       >
-        <Box as="span" id={ariaId}>
-          {label}
-        </Box>
-        {required && (
+        <Box
+          {...props}
+          ref={ref}
+          as={as}
+          type={type}
+          aria-labelledby={ariaId}
+          value={value}
+          defaultValue={defaultValue}
+          disabled={disabled}
+          onChange={wrapEvent(onChange, handleChange)}
+          placeholder={isDate ? "mm/dd/yyyy" : undefined}
+          sx={{
+            all: "unset",
+            appearance: "none",
+            p: icon ? "3rem 2rem 1rem 5rem" : "3rem 2rem 1rem",
+            boxSizing: "border-box",
+            position: "relative",
+            fontFamily: "body",
+            fontSize: "body",
+            lineHeight: "body",
+            width: "100%",
+            height: isTextarea ? "auto" : "6.5rem",
+            outline: "none",
+            clipPath: isTextarea ? "inset(3rem 0 0 0)" : "none",
+            "&::-webkit-calendar-picker-indicator": {
+              background: "transparent",
+              opacity: 1,
+              cursor: "pointer",
+            },
+            "&::-webkit-calendar-picker-indicator:hover + svg": {
+              color: "ui_300",
+            },
+          }}
+        />
+        {isDate && (
           <Box
-            as="span"
+            as={Calendar}
+            className="calendar-icon"
             sx={{
-              width: 6,
-              height: 6,
-              bg: "red_500",
-              borderRadius: "50%",
-              ml: "1rem",
+              pointerEvents: "none",
+              position: "absolute",
+              right: "2rem",
+              top: "3rem",
             }}
+          />
+        )}
+        <Box
+          className="input__label-row"
+          sx={{
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+            top: "2.25rem",
+            left: icon ? "5rem" : "2rem",
+            transformOrigin: "top left",
+            pointerEvents: "none",
+            color: disabled ? "ui_500" : "inherit",
+          }}
+        >
+          <Box as="span" id={ariaId}>
+            {label}
+          </Box>
+          {required && (
+            <Box
+              as="span"
+              sx={{
+                width: 6,
+                height: 6,
+                bg: "red_500",
+                borderRadius: "50%",
+                ml: "1rem",
+              }}
+            />
+          )}
+        </Box>
+        {icon && (
+          <Box
+            as={icon}
+            aria-label="input icon"
+            sx={{ position: "absolute", left: "2rem", top: "2.25rem" }}
           />
         )}
       </Box>
       {error && (
         <Box
-          as="span"
           role="alert"
           sx={{
             fontSize: "small",
+            lineHeight: "small",
             color: "red_500",
             width: "100%",
           }}
         >
           {error}
         </Box>
-      )}
-      {icon && (
-        <Box
-          as={icon}
-          aria-label="input icon"
-          sx={{ position: "absolute", left: "2rem", top: "2.25rem" }}
-        />
       )}
     </Box>
   )
