8f4e66722511bf9ff8df2b064aa924de28b6c29a
Patrick Krawczykowski <patrick@dreadful.design>
2021-01-11 16:02:54 -0600

    update story

--- packages/avatar/avatar.stories.js
+++ packages/avatar/avatar.stories.js
@@ -5,10 +5,19 @@ export default { title: "Avatar" }
 
 export function Default() {
   return (
+    // Any prop not defined here will be forwarded as expected
     <Avatar
+      // The size of an avatar is large by default
+      size="large"
+      // This can either be the LL name or their company name.
       name="Chicago Properties & Financials of Streeterville Coast"
+      // This is optional.
       email="janet.wooderhousen@email.com"
+      // This defaults to a "blank avatar" hosted on s3. So if you don't have a
+      // user photo, don't worry about it.
       photo="https://i.pravatar.cc/72"
+      // This won't be rendered unless the size is "small".
+      intitials="PK"
     />
   )
 }
