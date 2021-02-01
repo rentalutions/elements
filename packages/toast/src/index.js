e2ac1f4b33bfd8ef70531da888ad93bd53a25a64
Patrick Krawczykowski <patrick@dreadful.design>
2021-02-01 14:37:42 -0600

    remove feather-icon dependency

--- packages/toast/src/index.js
+++ packages/toast/src/index.js
@@ -10,7 +10,6 @@ import { createPortal } from "react-dom"
 import { Box } from "@rent_avail/layout"
 import { getId, usePortal } from "@rent_avail/utils"
 import { motion, AnimatePresence } from "framer-motion"
-import { Loader } from "react-feather"
 
 const ToastContext = createContext()
 
@@ -198,10 +197,8 @@ function ToastProvider({ position = "bottom-left", ...props }) {
                       alignItems: "center",
                       justifyConent: "center",
                     }}
-                    animate={{ rotate: 360 }}
-                    transition={{ repeat: Infinity, duration: 2 }}
                   >
-                    <Loader />
+                    <LoadingIcon />
                   </Box>
                 ) : null}
                 {returnValue(children, props)}
@@ -241,6 +238,33 @@ function useToast() {
     []
   )
   return toast
+}
+
+function LoadingIcon() {
+  return (
+    <motion.svg
+      animate={{ rotate: 360 }}
+      transition={{ repeat: Infinity, duration: 2 }}
+      xmlns="http://www.w3.org/2000/motion.svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="12" y1="2" x2="12" y2="6" />
+      <line x1="12" y1="18" x2="12" y2="22" />
+      <line x1="4.93" y1="4.93" x2="7.76" y2="7.76" />
+      <line x1="16.24" y1="16.24" x2="19.07" y2="19.07" />
+      <line x1="2" y1="12" x2="6" y2="12" />
+      <line x1="18" y1="12" x2="22" y2="12" />
+      <line x1="4.93" y1="19.07" x2="7.76" y2="16.24" />
+      <line x1="16.24" y1="7.76" x2="19.07" y2="4.93" />
+    </motion.svg>
+  )
 }
 
 export { ToastProvider, useToast }
