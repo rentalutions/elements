66b0763d305de2e52df7e5b2619e3aa83a8cd1f9
Patrick Krawczykowski <patrick@dreadful.design>
2021-05-12 16:45:57 -0500

    feat: update confirmation dialog to consume custom hook

--- packages/dialog/src/Confirm.js
+++ packages/dialog/src/Confirm.js
@@ -1,75 +1,93 @@
-import React, { useContext, forwardRef, Fragment, memo } from "react"
+import React, { useContext, forwardRef } from "react"
 import { createPortal } from "react-dom"
-import styled from "styled-components"
 import { usePortal } from "@rent_avail/utils"
 import { motion, AnimatePresence } from "framer-motion"
 import { DialogContext } from "./dialogContext"
 import { Box } from "@rent_avail/layout"
 
-const ConfirmationWrapper = styled(motion.section)`
-  position: absolute;
-  top: 4rem;
-  left: 50%;
-  padding: 2rem;
-  margin-bottom: 4rem;
-  margin-left: auto;
-  margin-right: auto;
-  background: ${({ theme }) => theme.colors.ui_100};
-  border-radius: 0.25rem;
-  width: calc(100% - 2rem);
-  max-width: 50rem;
-  z-index: 2;
-  transform: translate3d(-50%, 0, 0);
-  .confirmation__header {
-    display: flex;
-    margin-bottom: 2rem;
-  }
-  .confirmation__close {
-    cursor: pointer;
-    margin-left: auto;
-    &:hover {
-      opacity: 0.75;
-    }
-  }
-`
+const backdropVariants = {
+  hidden: { opacity: 0 },
+  shown: { opacity: 1 },
+}
 
-function Confirmation({ children, title = null, scrimSx = {}, ...props }, ref) {
+const dialogVariants = {
+  hidden: { opacity: 0, scale: 1.05, x: "-50%" },
+  shown: { opacity: 1, scale: 1, transition: { delay: 0.3 } },
+}
+
+function useConfirmationDialog({ ref, scrimSx, title, ...rest }) {
   const target = usePortal()
   const { open, id } = useContext(DialogContext)
+  return {
+    target,
+    open,
+    scrimProps: {
+      key: `${id}__scrim`,
+      as: motion.div,
+      variants: backdropVariants,
+      initial: "hidden",
+      animate: "shown",
+      exit: "hidden",
+    },
+    dialogProps: {
+      ...rest,
+      ref,
+      key: `${id}__confirmation`,
+      as: motion.aside,
+      variants: dialogVariants,
+      initial: "hidden",
+      animate: "shown",
+      exit: "hidden",
+    },
+  }
+}
+
+export const ConfirmationDialog = forwardRef(function ConfirmationDialog(
+  { sx = {}, scrimSx = {}, ...props },
+  ref
+) {
+  const { target, open, scrimProps, dialogProps } = useConfirmationDialog({
+    ...props,
+    ref,
+  })
   if (!target) return null
   return createPortal(
     <AnimatePresence>
       {open && (
-        <Fragment>
+        <Box
+          {...scrimProps}
+          sx={{
+            position: "fixed",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            background: "rgba(0, 0, 0, 0.24)",
+            overflow: "auto",
+            ...scrimSx,
+          }}
+        >
           <Box
-            className="scrim"
+            {...dialogProps}
             sx={{
-              position: "fixed",
-              top: 0,
-              right: 0,
-              bottom: 0,
-              left: 0,
-              background: "rgba(0, 0, 0, 0.24)",
-              overflow: "auto",
-              ...scrimSx
+              position: "absolute",
+              top: "4rem",
+              left: "50%",
+              p: "2rem",
+              mx: "auto",
+              mb: "2rem",
+              bg: "ui_100",
+              borderRadius: 4,
+              width: "calc(100% - 4rem)",
+              maxWidth: "50rem",
+              zIndex: 2,
+              transform: "translate3d(-50%, 0, 0)",
+              ...sx,
             }}
-          >
-            <ConfirmationWrapper
-              {...props}
-              key={`${id}-dialog`}
-              ref={ref}
-              initial={{ opacity: 0, scale: 1.05, x: "-50%" }}
-              animate={{ opacity: 1, scale: 1, transition: { delay: 0.3 } }}
-              exit={{ opacity: 0, scale: 1.05 }}
-            >
-              {children}
-            </ConfirmationWrapper>
-          </Box>
-        </Fragment>
+          />
+        </Box>
       )}
     </AnimatePresence>,
     target
   )
-}
-
-export const ConfirmationDialog = memo(forwardRef(Confirmation))
+})
