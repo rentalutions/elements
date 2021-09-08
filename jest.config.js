e8db92f709956a4d8efbbcc9f43d96e2a5130507
Patrick Krawczykowski <patrick@dreadful.design>
2021-09-08 14:54:12 -0500

    update tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testPathIgnorePatterns: ["/node_modules/", "/template/"],
   transformIgnorePatterns: ["/node_modules/", "<rootDir>/packages/*./dist/"],
+  testEnvironment: "jsdom",
   moduleNameMapper: {
     "#testing": "<rootDir>/utils/testing.js",
   },
