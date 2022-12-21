5873b113aefe2ba64eaa1fc2e1a68d7ab6350d74
pkrawc <patrick@dreadful.design>
2022-12-21 11:32:53 -0600

    restore layout customization

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,36 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+import React from "react"
+import { ElementsProvider } from "@rent_avail/core"
+
+export const decorators = [
+  (Story) => {
+    return (
+      <ElementsProvider>
+        <Story />
+      </ElementsProvider>
+    )
   },
+]
+
+]
+
+export const parameters = {
+  layout: "fullscreen",
+  backgrounds: {
+    default: "light",
+    values: [
+      { name: "app", value: "#f3f3f3" },
+      { name: "light", value: "#fff" },
+      { name: "dark", value: "#2d2d2d" },
+    ],
+  }
 }
+
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// }
