f300bec88924d7307c3ca3030eb785296ee8162a
pkrawc <patrick@dreadful.design>
2023-01-04 17:09:51 -0600

    customize storybook and edit package architecture

---
+++ .storybook/preview.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { Table } from "@storybook/components"
+import { ElementsProvider, baseTheme } from "@rent_avail/core"
+import { storybookTheme } from "./manager"
+
+export const decorators = [
+  (Story) => {
+    return (
+      <ElementsProvider>
+        <Story />
+      </ElementsProvider>
+    )
+  },
+]
+
+export const parameters = {
+  docs: {
+    theme: storybookTheme,
+    components: {
+      table: (props) => <Table style={{ width: "100%" }} {...props} />,
+    },
+  },
+  layout: "fullscreen",
+  backgrounds: {
+    default: "App Background",
+    values: [
+      { name: "App Background", value: baseTheme.colors.ui_300 },
+      { name: "Light", value: baseTheme.colors.ui_100 },
+      { name: "Dark", value: baseTheme.colors.ui_900 },
+    ],
+  },
+  options: {
+    storySort: {
+      order: ["Intro", "Packages", "Patterns"],
+    },
+  },
+}
