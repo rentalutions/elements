b6979a97be7bd0583a540e983aa1458fb1217f8c
Jeremy Grubard <jgrubard@gmail.com>
2024-03-01 15:37:58 -0500

    [RENA-6114] cleans up ui.

--- packages/icons/stories/default.story.js
+++ packages/icons/stories/default.story.js
@@ -1,5 +1,5 @@
 import * as icons from "../src"
-import { Card, Flex, Box, Container } from "@rent_avail/core"
+import { Flex, Box, Container, baseTheme } from "@rent_avail/core"
 
 const iconsArray = []
 
@@ -9,31 +9,30 @@ for (let icon in icons) {
 
 export function Icons() {
   return (
-    <Container sx={{ pt: "2rem" }}>
-      <Card as={Flex} sx={{ p: "2rem", my: "2rem", flexWrap: "wrap", justifyContent: "center" }}>
-        {iconsArray.map((icon) => (
-          <Flex
-            key={icon.displayName}
-            title={icon.displayName}
-            sx={{
-              m: "1rem",
-              p: "1rem",
-              width: "10rem",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              bg: "ui_300",
-              borderRadius: "4px",
-              "&:hover": {
-                bg: "ui_500",
-              }
-            }}
-          >
-            <Box as={icon} />
-            <Box sx={{ text: "small", fontSize: "1rem", mt: "1rem" }}>{icon.displayName}</Box>
-          </Flex>
-        ))}
-      </Card>
+    <Container as={Flex} sx={{ p: "2rem", my: "2rem", flexWrap: "wrap", justifyContent: "center" }}>
+      {iconsArray.map((icon) => (
+        <Flex
+          key={icon.displayName}
+          title={icon.displayName}
+          sx={{
+            m: "1rem",
+            p: "1rem",
+            width: "10rem",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            bg: "ui_100",
+            borderRadius: "4px",
+            boxShadow: `0px 1px 5px ${baseTheme.colors.ui_500}`,
+            "&:hover": {
+              boxShadow: `0px 1px 5px ${baseTheme.colors.ui_700}`,
+            }
+          }}
+        >
+          <Box as={icon} />
+          <Box sx={{ text: "small", fontSize: "1rem", mt: "1rem" }}>{icon.displayName}</Box>
+        </Flex>
+      ))}
     </Container>
   )
 }
