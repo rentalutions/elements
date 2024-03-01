d99c1c94a7521df91cb3a0e86136253b5f9ec7e2
Jeremy Grubard <jgrubard@gmail.com>
2024-03-01 15:01:35 -0500

    [RENA-6114] update default story for icon to include display name and adds some basic styling to keep icons uniform and not crashing into each other with the added component names.

--- packages/icons/stories/default.story.js
+++ packages/icons/stories/default.story.js
@@ -1,5 +1,5 @@
 import * as icons from "../src"
-import { Box, Container } from "@rent_avail/core"
+import { Card, Flex, Box, Container } from "@rent_avail/core"
 
 const iconsArray = []
 
@@ -10,9 +10,30 @@ for (let icon in icons) {
 export function Icons() {
   return (
     <Container sx={{ pt: "2rem" }}>
-      {iconsArray.map((icon) => (
-        <Box as={icon} sx={{ m: "1rem" }} />
-      ))}
+      <Card as={Flex} sx={{ p: "2rem", my: "2rem", flexWrap: "wrap", justifyContent: "center" }}>
+        {iconsArray.map((icon) => (
+          <Flex
+            key={icon.displayName}
+            title={icon.displayName}
+            sx={{
+              m: "1rem",
+              p: "1rem",
+              width: "10rem",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              bg: "ui_300",
+              borderRadius: "4px",
+              "&:hover": {
+                bg: "ui_500",
+              }
+            }}
+          >
+            <Box as={icon} />
+            <Box sx={{ text: "small", fontSize: "1rem", mt: "1rem" }}>{icon.displayName}</Box>
+          </Flex>
+        ))}
+      </Card>
     </Container>
   )
 }
