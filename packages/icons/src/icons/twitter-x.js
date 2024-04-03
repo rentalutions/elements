b497fdd8431ca7f78440908a3f2d147aab000769
Mihail <azimutcorp@gmail.com>
2024-04-03 15:07:47 -0700

    RENA-6059 remove currentColor

--- packages/icons/src/icons/twitter-x.js
+++ packages/icons/src/icons/twitter-x.js
@@ -1,10 +1,13 @@
 import React, { forwardRef } from "react"
 import p from "prop-types"
 export const TwitterX = forwardRef(function TwitterX(props, ref) {
-  const { color = "currentColor", size = 24, ...rest } = props
+  const { size = 24, color = "currentColor", ...rest } = props
   return (
-    <svg  {...rest} ref={ref} stroke={color} width={size} height={size} viewBox="0 0 1200 1227" xmlns="http://www.w3.org/2000/svg">
-      <path d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z" />
+    <svg  {...rest} ref={ref} width={size} height={size} viewBox="0 0 1200 1227" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
+        fill={color}
+      />
     </svg>
   )
 })
@@ -15,11 +18,7 @@ TwitterX.propTypes = {
 }
 
 TwitterX.defaultProps = {
-  viewBox: "0 0 24 24",
-  fill: "none",
-  strokeWidth: 2,
-  strokeLinecap: "round",
-  strokeLinejoin: "round",
+  viewBox: "0 0 1200 1227",
 }
 
 TwitterX.displayName = "TwitterX"
